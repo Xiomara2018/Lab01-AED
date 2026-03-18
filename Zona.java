2fe687a868d0336496c1f72c0f3a74b2e185b77a
Xiomara2018 <xiomaranunezccupa@gmail.com>
2026-03-18 06:48:48 -0500

    Clase Zona

--- Zona.java
+++ Zona.java
@@ -1,1 +1,26 @@
+public class Zona {
+    private String mineral;
+    private int cantidad;
+    private double puereza;
 
+    public Zona(String mineral, int cantidad, double puerza)
+    {
+        this.mineral = mineral;
+        this.cantidad = cantidad;
+        this.puereza = puerza;
+    }
+    public double calcularValor() {
+        return this.cantidad * this.puereza;
+    }
+    public String getMineral() {
+        return mineral;
+    }
+    @Override
+    public String toString() {
+        return "Zona{" +
+                "mineral='" + mineral + '\'' +
+                ", cantidad=" + cantidad +
+                ", puereza=" + puereza +
+                '}';
+    }
+}
